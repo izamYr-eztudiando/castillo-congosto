7f176a673aa0a7bb38dc97cf2621ff725d18bec6
izamYr-eztudiando <isamirvalencia@centroculturalsalmantino.com>
2025-06-05 15:04:16 +0200

    duodecimo commit

--- pcastillocongosto/appcastillocongosto/views.py
+++ pcastillocongosto/appcastillocongosto/views.py
@@ -2,6 +2,9 @@ from __future__ import unicode_literals
 from django.shortcuts import render, redirect, get_object_or_404
 from appcastillocongosto.models import *
 from django.http import HttpResponse
+
+# Paginación
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 # Create your views here.
 DEBUG = True
 
@@ -48,6 +51,36 @@ def chat(request):
     numcontactos = Contacto.objects.count()
     numleidos = Contacto.objects.filter(leido=False).count()
     numcontestados = Contacto.objects.filter(contestado=False).count()
+    # Paginación de los contactos, 10 contactos por página
+    page = request.GET.get('page')
+    paginator = Paginator(lista_contactos, 10)
+    if page == None:
+        print(" page recibe fuera de get o post NONE ="+ str(page))
+        page = paginator.num_pages
+        request.session["pagina"] = page
+    else:
+        print(" page recibe este del nonede geo o post="+ str(page))
+        request.session["pagina"] = page
+    
+    if request.method == 'GET':
+        pagina = request.session["pagina"]
+        print(" page recibe en GET=" + str(pagina))
+    if request.method == 'POST':
+        pagina = request.session["pagina"]
+        print(" page recibe en POST= " +str(pagina))
+
+    # condición muy importante para saber si existe la varible en la sesión
+    if "pagina" in request.session:
+        page = request.session["pagina"]
+        print(" page recibe de sesion=" +str(page))
+
+    try:
+        lista_contactos = paginator.get_page(page)
+    except PageNotAnInteger: 
+        lista_contactos = paginator.page(1)
+    except EmptyPage:
+        lista_contactos = paginator.page(paginator.num_pages)
+
     if request.method == 'POST':
         return redirect('chat')
     context = {'lista_contactos': lista_contactos, 'numleidos': numleidos, 'numcontestados': numcontestados, 'numcontactos': numcontactos}
